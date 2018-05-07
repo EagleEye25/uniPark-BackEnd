a7107ff159a00b2835668af5fe66246b567ce75f
Daniel Maree <danieldiewindpomp@gmail.com>
2018-05-07 16:46:12 +0200

    Models
    took out reference on dbaccess

--- uniPark-DAL/DBAccess.cs
+++ uniPark-DAL/DBAccess.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
 //using TypeLib.Interfaces;
-using TypeLib.Models;
+//using TypeLib.Models;
 //using TypeLib.ViewModels;
 
 namespace uniPark_DAL
