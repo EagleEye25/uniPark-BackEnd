ff63f9f6418acc88916acd44de7987aeacc28850
Daniel Maree <danieldiewindpomp@gmail.com>
2018-05-07 15:00:54 +0200

    Updated architecture
    Modified the architecture to incorporate ntier

---
+++ uniPark-DAL/DBAccess.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+//using TypeLib.Interfaces;
+//using TypeLib.Models;
+//using TypeLib.ViewModels;
+
+namespace uniPark_DAL
+{
+    class DBAccess
+    {
+    }
+}
