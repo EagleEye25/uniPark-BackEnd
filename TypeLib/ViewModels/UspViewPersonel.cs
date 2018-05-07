5d537ed9ef8831737eb30cd78df0efba0f5ff690
Daniel Maree <danieldiewindpomp@gmail.com>
2018-05-07 17:27:01 +0200

    ViewModels
    Added viewmodels

---
+++ TypeLib/ViewModels/UspViewPersonel.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypeLib.ViewModels
+{
+    public class UspViewPersonel
+    {
+        public string PersonelID { get; set; }
+        public string Name { get; set; }
+    }
+}
